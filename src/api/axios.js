a8cd30ee5c3506cb8d1107303e738e304c7a3f85
SOWPARNIKA M <sowparnikasatheesh248@gmail.com>
2025-06-30 16:08:25 +0530

    first commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "https://mern-backend-98xl.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3001/api",
   withCredentials: true,
 });
 
