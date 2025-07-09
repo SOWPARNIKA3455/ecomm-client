5c2adf36d20891c6040b32a8c4e9a3d8e22483fe
SOWPARNIKA M <sowparnikasatheesh248@gmail.com>
2025-07-09 23:09:08 +0530

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -272,7 +272,7 @@ const Navbar = () => {
       )}
 
       {/* Desktop Categories */}
-      <nav className="hidden md:flex bg-yellow-800 dark:bg-blue-900 text-white px-4 py-2 overflow-x-auto whitespace-nowrap">
+      <nav className=" bg-yellow-800 dark:bg-blue-900 text-white px-4 py-2 overflow-x-auto whitespace-nowrap">
         <ul className="flex items-center space-x-6 text-sm">
           {categories.map((cat) => (
             <li
